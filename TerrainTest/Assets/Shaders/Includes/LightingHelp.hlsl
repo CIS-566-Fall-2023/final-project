21c2936338dfca6bbb446f24882c0cc5542bae16
Janet Wang <55120659+xchennnw@users.noreply.github.com>
2023-11-14 21:23:21 -0500

    unity project

---
+++ TerrainTest/Assets/Shaders/Includes/LightingHelp.hlsl
@@ -0,0 +1,33 @@
+void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction, out float DistanceAtten, out float ShadowAtten) {
+#ifdef SHADERGRAPH_PREVIEW
+    Direction = normalize(float3(0.5, 0.5, 0));
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+    #if SHADOWS_SCREEN
+        float4 clipPos = TransformWorldToClip(WorldPos);
+        float4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+        float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
+#endif
+}
+
+void ChooseColor_float(float3 Highlight, float3 Shadow, float Diffuse, float Threshold, out float3 OUT)
+{
+    if (Diffuse < Threshold)
+    {
+        OUT = Shadow;
+    }
+    else
+    {
+        OUT = Highlight;
+    }
+}
