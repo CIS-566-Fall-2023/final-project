1d581064cd4c56c4bf03703671d16b2847f96559
Nicholas Liu <liunicholas6@gmail.com>
2023-12-03 17:35:20 -0500

    Curve to point stream

--- Assets/Scripts/Geom/ICurve.cs
+++ Assets/Scripts/Geom/ICurve.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Geom
@@ -8,5 +9,18 @@ namespace Geom
         Vector2 Point(float t);
         (ICurve, Vector2, ICurve) Split(float t);
         ICurve Reverse();
+
+        IEnumerable<Vector2> ToPointStream(float delta = 1)
+        {
+            var length = Length();
+            var segmentCount = (int)Mathf.Ceil(delta * length);
+            var segmentLength = length / segmentCount;
+            for (var i = 0; i < segmentCount; i++)
+            {
+                yield return Point(i * segmentLength);
+            }
+        }
     }
+    
+
 }
