dab7287144a577163b940f43bc3e52bfc7f4f41c
Nicholas Liu <liunicholas6@gmail.com>
2023-12-03 22:27:11 -0500

    Moved rectangle; created sector and sector region

---
+++ Assets/Scripts/Geom/Sector.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Geom
+{
+    public class Sector
+    {
+        public Vector2 Center;
+        public float RadiusInner;
+        public float RadiusOuter;
+        public float Theta0;
+        public float Theta1;
+        
+    }
+}
