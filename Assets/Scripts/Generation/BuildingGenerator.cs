35239e9c03ae285d59e6c45b3293c2a5d063d0b9
Nicholas Liu <liunicholas6@gmail.com>
2023-12-05 18:35:20 -0500

    Tower Complete

--- Assets/Scripts/Generation/BuildingGenerator.cs
+++ Assets/Scripts/Generation/BuildingGenerator.cs
@@ -25,12 +25,12 @@ namespace Generation
 
         public void GenerateBuilding()
         {
-            var runner = new PartitionRunner(this, new Rectangle
-            {
-                Min = new Vector2(-100, -50), Max = new Vector2(100, 50)
-            });
-            runner.Run();
-            //var runner = new TowerRunner(this, new Vector2(0, 0), 10, 20);
+            // var runner = new PartitionRunner(this, new Rectangle
+            // {
+            //     Min = new Vector2(-100, -50), Max = new Vector2(100, 50)
+            // });
+            // runner.Run();
+            var runner = new TowerRunner(this, new Vector2(0, 0), 10, 20);
         }
     }
 }
