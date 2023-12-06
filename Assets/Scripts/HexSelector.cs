edcef96e62b161e066332f8807b3b12891219f4f
Caballo <1076318970@qq.com>
2023-12-06 17:27:30 +0800

    Prehex

--- Assets/Scripts/HexSelector.cs
+++ Assets/Scripts/HexSelector.cs
@@ -2,6 +2,7 @@ using Planetile;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class HexSelector : MonoBehaviour
 {
@@ -12,7 +13,21 @@ public class HexSelector : MonoBehaviour
 
     private GameObject lastHoveredHex;
     [SerializeField] GameObject preHex;
+    public List<GameObject> Items1 = new List<GameObject>();
+    public List<GameObject> Items2 = new List<GameObject>();
+    public List<GameObject> Items3 = new List<GameObject>();
+    public List<GameObject> Items4 = new List<GameObject>();
+    public List<GameObject> Items5 = new List<GameObject>();
+    public List<GameObject> Items6 = new List<GameObject>();
+    public List<GameObject> Items7 = new List<GameObject>();
+    public List<GameObject> Items8 = new List<GameObject>();
+    public List<GameObject> Items9 = new List<GameObject>();
+    public List<GameObject> Items10 = new List<GameObject>();
+    public List<GameObject> Items11 = new List<GameObject>();
+    public List<GameObject> Items12 = new List<GameObject>();
+
     private HexagonTile _hex;
+    bool hasInit = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,26 +35,7 @@ public class HexSelector : MonoBehaviour
 
         //preHex.AddComponent<HexagonTile>();
         _hex = preHex.GetComponent<HexagonTile>();
-
-        WFCType type1 = (WFCType)(0x01 << SimpleHash(1 + Random.Range(0, 1000)));
-        WFCType type2 = (WFCType)(0x01 << SimpleHash(2 + Random.Range(0, 1000)));
-        WFCType type3 = (WFCType)(0x01 << SimpleHash(3 + Random.Range(0, 1000)));
-        WFCType type4 = (WFCType)(0x01 << SimpleHash(4 + Random.Range(0, 1000)));
-        WFCType type5 = (WFCType)(0x01 << SimpleHash(5 + Random.Range(0, 1000)));
-        WFCType type6 = (WFCType)(0x01 << SimpleHash(6 + Random.Range(0, 1000)));
-
-        _hex.cellData.CellsOnTileEdge0[0].Type = type1;
-        _hex.cellData.CellsOnTileEdge0[1].Type = type1;
-        _hex.cellData.CellsOnTileEdge1[0].Type = type2;
-        _hex.cellData.CellsOnTileEdge1[1].Type = type2;
-        _hex.cellData.CellsOnTileEdge2[0].Type = type3;
-        _hex.cellData.CellsOnTileEdge2[1].Type = type3;
-        _hex.cellData.CellsOnTileEdge3[0].Type = type4;
-        _hex.cellData.CellsOnTileEdge3[1].Type = type4;
-        _hex.cellData.CellsOnTileEdge4[0].Type = type5;
-        _hex.cellData.CellsOnTileEdge4[1].Type = type5;
-        _hex.cellData.CellsOnTileEdge5[0].Type = type6;
-        _hex.cellData.CellsOnTileEdge5[1].Type = type6;
+        RefreshHex();
     }
 
     private int SimpleHash(int input)
@@ -58,6 +54,62 @@ public class HexSelector : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    private void RefreshHex()
+    {
+        int t1 = SimpleHash(1 + Random.Range(0, 1000));
+        int t2 = SimpleHash(2 + Random.Range(0, 1000));
+        int t3 = SimpleHash(3 + Random.Range(0, 1000));
+        int t4 = SimpleHash(4 + Random.Range(0, 1000));
+        int t5 = SimpleHash(5 + Random.Range(0, 1000));
+        int t6 = SimpleHash(6 + Random.Range(0, 1000));
+
+        WFCType type1 = (WFCType)(0x01 << t1);
+        WFCType type2 = (WFCType)(0x01 << t2);
+        WFCType type3 = (WFCType)(0x01 << t3);
+        WFCType type4 = (WFCType)(0x01 << t4);
+        WFCType type5 = (WFCType)(0x01 << t5);
+        WFCType type6 = (WFCType)(0x01 << t6);
+
+        _hex.cellData.CellsOnTileEdge0[0].Type = type1;
+        ActiveItem(Items1, t1);
+        _hex.cellData.CellsOnTileEdge0[1].Type = type1;
+        ActiveItem(Items2, t1);
+
+        _hex.cellData.CellsOnTileEdge1[0].Type = type2;
+        ActiveItem(Items3, t2);
+        _hex.cellData.CellsOnTileEdge1[1].Type = type2;
+        ActiveItem(Items4, t2);
+
+        _hex.cellData.CellsOnTileEdge2[0].Type = type3;
+        ActiveItem(Items5, t3);
+        _hex.cellData.CellsOnTileEdge2[1].Type = type3;
+        ActiveItem(Items6, t3);
+
+        _hex.cellData.CellsOnTileEdge3[0].Type = type4;
+        ActiveItem(Items7, t4);
+        _hex.cellData.CellsOnTileEdge3[1].Type = type4;
+        ActiveItem(Items8, t4);
+
+        _hex.cellData.CellsOnTileEdge4[0].Type = type5;
+        ActiveItem(Items9, t5);
+        _hex.cellData.CellsOnTileEdge4[1].Type = type5;
+        ActiveItem(Items10, t5);
+
+        _hex.cellData.CellsOnTileEdge5[0].Type = type6;
+        ActiveItem(Items11, t6);
+        _hex.cellData.CellsOnTileEdge5[1].Type = type6;
+        ActiveItem(Items12, t6);
+    }
+
+    public void ActiveItem(List<GameObject> Items, int i)
+    {
+        foreach (GameObject item in Items)
+        {
+            item.SetActive(false);
+        }
+        Items[i].SetActive(true);
+    }
 
     void Update()
     {
@@ -121,25 +173,7 @@ public class HexSelector : MonoBehaviour
 
                     WFCManager.Instance.FillOneTile(selected);
 
-                    WFCType type1 = (WFCType)(0x01 << SimpleHash(1 + Random.Range(0, 1000)));
-                    WFCType type2 = (WFCType)(0x01 << SimpleHash(2 + Random.Range(0, 1000)));
-                    WFCType type3 = (WFCType)(0x01 << SimpleHash(3 + Random.Range(0, 1000)));
-                    WFCType type4 = (WFCType)(0x01 << SimpleHash(4 + Random.Range(0, 1000)));
-                    WFCType type5 = (WFCType)(0x01 << SimpleHash(5 + Random.Range(0, 1000)));
-                    WFCType type6 = (WFCType)(0x01 << SimpleHash(6 + Random.Range(0, 1000)));
-
-                    _hex.cellData.CellsOnTileEdge0[0].Type = type1;
-                    _hex.cellData.CellsOnTileEdge0[1].Type = type1;
-                    _hex.cellData.CellsOnTileEdge1[0].Type = type2;
-                    _hex.cellData.CellsOnTileEdge1[1].Type = type2;
-                    _hex.cellData.CellsOnTileEdge2[0].Type = type3;
-                    _hex.cellData.CellsOnTileEdge2[1].Type = type3;
-                    _hex.cellData.CellsOnTileEdge3[0].Type = type4;
-                    _hex.cellData.CellsOnTileEdge3[1].Type = type4;
-                    _hex.cellData.CellsOnTileEdge4[0].Type = type5;
-                    _hex.cellData.CellsOnTileEdge4[1].Type = type5;
-                    _hex.cellData.CellsOnTileEdge5[0].Type = type6;
-                    _hex.cellData.CellsOnTileEdge5[1].Type = type6;
+                    RefreshHex();
                 }
             }
         }
