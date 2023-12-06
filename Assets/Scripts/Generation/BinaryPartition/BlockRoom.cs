e4aa85868b90bb71695c936b72cf3d0c2bea687f
Nicholas Liu <liunicholas6@gmail.com>
2023-12-05 19:06:54 -0500

    Room walls have thickness

--- Assets/Scripts/Generation/BinaryPartition/BlockRoom.cs
+++ Assets/Scripts/Generation/BinaryPartition/BlockRoom.cs
@@ -14,10 +14,13 @@ namespace BinaryPartition
         private static readonly Vector2 TrimVec = new Vector2(Trim, Trim);
         private const float DoorMargin = 4;
         private const float DoorSize = 2;
+        private const float WallThickness = 4;
+        private static readonly Vector2 WallVec = new Vector2(WallThickness, WallThickness);
 
         private readonly DividerBounds[] _dividers;
-        private Rectangle _bigRect;
-        private Rectangle _smallRect;
+        private Rectangle _hallRect;
+        private Rectangle _innerRect;
+        private Rectangle _outerRect;
         private readonly BuildingGenerator _generator;
         private readonly VertexId _roomVert;
 
@@ -26,14 +29,19 @@ namespace BinaryPartition
         public BlockRoom(DividerBounds[] dividers, Rectangle rectangle, BuildingGenerator generator)
         {
             _dividers = dividers;
-            _bigRect = rectangle;
-            _smallRect = new Rectangle
+            _hallRect = rectangle;
+            _outerRect = new Rectangle
             {
-                Min = _bigRect.Min + TrimVec,
-                Max = _bigRect.Max - TrimVec,
+                Min = _hallRect.Min + TrimVec,
+                Max = _hallRect.Max - TrimVec,
+            };
+            _innerRect = new Rectangle
+            {
+                Min = _outerRect.Min + WallVec,
+                Max = _outerRect.Max - WallVec,
             };
             _generator = generator;
-            _roomVert = generator.Builder.MakeVertex(new VertexInfo(new RectangleRegion(_smallRect), VertexTag.Room));
+            _roomVert = generator.Builder.MakeVertex(new VertexInfo(new RectangleRegion(_innerRect), VertexTag.Room));
         }
 
         public void MakeWallsAndDoors()
@@ -47,30 +55,47 @@ namespace BinaryPartition
         private void MakeWall(Direction direction)
         {
             var divider = direction.GetDivider(_dividers);
-            var (low, high) = direction.GetRectangleSide(_smallRect);
+            var (lowInner, highInner) = direction.GetRectangleSide(_innerRect);
+            var (lowOuter, highOuter) = direction.GetRectangleSide(_outerRect);
 
             if (divider == null || Random.value < 0.2)
             {
-                _generator.AddWall(new LineCurve(low, high));
+                _generator.AddWall(new LineCurve(lowInner, highInner));
+                _generator.AddWall(new LineCurve(lowOuter, highOuter));
                 return;
             }
             
             var perp = direction.Perp().ToVector();
-            var doorPosition = Vector2.Lerp(
-                low + perp * DoorMargin,
-                high - perp * DoorMargin,
-                Random.value);
             
-            _generator.AddWall(new LineCurve(low, doorPosition - perp * DoorSize));
-            _generator.AddWall(new LineCurve(high, doorPosition + perp * DoorSize));
+            var doorInnerPosition = Vector2.Lerp(
+                lowInner + perp * (WallThickness + DoorMargin),
+                highInner - perp * (WallThickness + DoorMargin),
+                Random.value);
 
-            var hallPosition = doorPosition + direction.ToVector() * Trim;
+            var lowInnerDoorPosition = doorInnerPosition - perp * DoorSize;
+            var highInnerDoorPosition = doorInnerPosition + perp * DoorSize;
+
+            var doorOuterPosition = doorInnerPosition + direction.ToVector() * WallThickness;
+
+            var lowOuterDoorPosition = doorOuterPosition - perp * DoorSize;
+            var highOuterDoorPosition = doorOuterPosition + perp * DoorSize;
+            
+            _generator.AddWall(new LineCurve(lowInner, lowInnerDoorPosition));
+            _generator.AddWall(new LineCurve(highInner, highInnerDoorPosition));
+            
+            _generator.AddWall(new LineCurve(lowOuter, lowOuterDoorPosition));
+            _generator.AddWall(new LineCurve(highOuter, highOuterDoorPosition));
+            
+            _generator.AddWall(new LineCurve(lowOuterDoorPosition, lowInnerDoorPosition));
+            _generator.AddWall(new LineCurve(highInnerDoorPosition, highOuterDoorPosition));
+
+            var hallPosition = doorInnerPosition + direction.ToVector() * (Trim + WallThickness);
             var hallVert = Builder.MakeVertex(hallPosition);
             
             Builder.MakeEdge(hallVert, _roomVert, EdgeTag.Doorway,
                 new LineCurve(
                     hallPosition,
-                    doorPosition
+                    doorInnerPosition
             ));
 
             switch (direction)
